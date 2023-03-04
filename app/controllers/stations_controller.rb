c0e4829d3ed1653d313779b67fe50b85b0624bcc
ionajosephine <ionajosephine@gmail.com>
2023-03-04 15:28:35 +0100

    Add station show view

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,4 +3,8 @@ class StationsController < ApplicationController
   def index 
     @stations = TideServices::Stations.new.call.body
   end
+
+  def show
+    @station = TideServices::Station.new(params[:id]).call.body
+  end
 end
