89ff89ab887b5cfd9dc52216264a8b1ee38847b1
ionajosephine <ionajosephine@gmail.com>
2023-03-18 19:44:10 +0100

    Add geocoder gem for adding station to spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,14 @@
 class Spot < ApplicationRecord
   belongs_to :user
+  belongs_to :station, optional: true
   validates :latitude, :longitude, :name, presence: true
+
+  before_save :set_station
+
+  private
+
+  def set_station
+    # The `.near` method comes from the geocoder gem
+    self.station = Station.near([latitude, longitude]).first
+  end
 end
