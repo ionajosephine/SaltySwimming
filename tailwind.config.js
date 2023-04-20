4745c65417eb6799d9b2f1d04de660a787c8ade4
ionajosephine <ionajosephine@gmail.com>
2023-04-20 12:32:23 +0200

    Add Tailwind Forms and custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'class', // only generate classes
+    })
   ]
 }
