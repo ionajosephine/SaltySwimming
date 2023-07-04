0aefb284bd354627dff311d87b1791c5d2c131fa
ionajosephine <81577041+ionajosephine@users.noreply.github.com>
2023-07-04 17:48:41 +0100

    Display 7 day forecast for Hayle

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,6 +2,7 @@ class WeatherController < ApplicationController
   def index 
     latitude = 50.186014
     longitude = -5.423444
-    @dailylula = WeatherServices::Daily.new.call(latitude, longitude)
+    dailylula = WeatherServices::Daily.new.call(latitude, longitude)
+    @daily_summary = dailylula.body["features"].first["properties"]
   end
 end
